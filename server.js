5e0053a20f13d56e49b33144ea58b14965ce6900
ivandevp <medinadiazivan@gmail.com>
2019-08-22 18:15:29 -0500

    Add server with connection and routes

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const router = require('./routes');
 
+dotenv.config();
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).end('Hola Bedu!');
-});
+async function run(app) {
+  await mongoose.connect(process.env.MONGO_DB_CONNECTION, {useNewUrlParser: true});
 
-app.listen(port, () => console.log(`Servidor listo en http://localhost:${port}`));
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }));
+  
+  // parse application/json
+  app.use(bodyParser.json());
+
+  app.use('/api/v1/posts', router);
+
+  app.get('/', (req, res) => {
+    res.status(200).end('Hola Bedu!');
+  });
+  
+  app.listen(port, () => console.log(`Servidor listo en http://localhost:${port}`));
+}
+
+run(app);
