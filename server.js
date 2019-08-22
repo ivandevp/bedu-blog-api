a56d620b578c834aa3efda74d97931ddca02cb25
ivandevp <medinadiazivan@gmail.com>
2019-08-22 15:11:50 -0500

    Initial setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).end('Hola Bedu!');
+});
+
+app.listen(port, () => console.log(`Servidor listo en http://localhost:${port}`));
